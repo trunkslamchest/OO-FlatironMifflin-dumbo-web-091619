20f7f3a30fb67ff39536b85811f7b1f1e78bd689
Austin Smith <trunk9slamchest@gmail.com>
2019-09-19 20:13:49 +0000

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,36 @@
 class Manager
 
+	attr_reader :name
+	attr_accessor :department, :age
+
+	@@all = []
+
+	def initialize(name, department, age)
+		@name = name
+		@department = department
+		@age = age
+		@@all << self
+	end
+
+	def employees
+		Employee.all.select { |worker| worker.manager_name == name }
+	end
+
+	def hire_employee(name, salary)
+		employees << Employee.new(name, salary, manager_name = self.name)
+	end
+
+	def self.all_departments
+		Manager.all.map {|field| field.department }
+	end
+
+	def self.average_age
+		sum = 0
+			Manager.all.map {|manager| sum += manager.age }
+		sum / Manager.all.length
+	end
+
+	def self.all
+		@@all
+	end
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,27 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+emp1 = Employee.new("One Person", 40000, "One Manager")
+emp2 = Employee.new("Two Person", 41000, "One Manager")
+emp3 = Employee.new("Three Person", 42000, "One Manager")
+emp4 = Employee.new("Four Person", 43000, "Two Manager")
+emp5 = Employee.new("Five Person", 56000, "Two Manager")
+emp6 = Employee.new("Six Person", 38000, "Two Manager")
+emp7 = Employee.new("Seven Person", 37000, "Three Manager")
+emp8 = Employee.new("Eight Person", 35000, "Three Manager")
+emp9 = Employee.new("Nine Person", 36000, "Three Manager")
+emp10 = Employee.new("Ten Person", 39000, "Four Manager")
+emp11 = Employee.new("Eleven Person", 50000, "Four Manager")
+emp12 = Employee.new("Twelve Person", 51000, "Four Manager")
 
-#Test your code here
 
+mang1 = Manager.new("One Manager", "Sales", 55)
+mang2 = Manager.new("Two Manager", "Public Relations", 50)
+mang3 = Manager.new("Three Manager", "Communications", 45)
+mang4 = Manager.new("Four Manager", "Finance", 40)
 
-binding.pry
 puts "done"
+
+# binding.pry
+
+# 0
